a109bfb8f9dea6e6fc698a5a2f451f041fc6ab68
theresaswayne <tcs6@columbia.edu>
2018-11-15 09:35:29 -0500

    Initial commit

---
+++ qbic_processing.R
@@ -0,0 +1,13 @@
+# qbic_processing.R
+# for reading CellProfiler data from nuclear analyses
+
+library(readr)
+qbic_C1DNANuclei <- read_csv("~/Desktop/output/run7 corr/qbic_C1DNANuclei.csv")
+
+#dnaTotalInt <- qbic_C1DNANuclei$Intensity_IntegratedIntensity_Channel1DNA
+#EdUMeanInt <- qbic_C1DNANuclei$Intensity_MeanIntensity_Channel2EdU
+dnaTotalInt <- qbic_C1DNANuclei$Intensity_IntegratedIntensity_Channel1DNACorr
+EdUMeanInt <- qbic_C1DNANuclei$Intensity_MeanIntensity_Channel2EdUCorr
+
+plot(x = dnaTotalInt, y = EdUMeanInt) # basic scatter plot
+
