1690f03ad9c58b865c6878a3cc204f0ff8aaa5a7
theresaswayne <tcs6@columbia.edu>
2018-11-15 17:04:13 -0500

    Update qbic_processing.R

--- qbic_processing.R
+++ qbic_processing.R
@@ -1,7 +1,9 @@
 # qbic_processing.R
-# for reading CellProfiler data from nuclear analyses
+# for examining CellProfiler data from nuclear analyses
 
+library(dplyr)
 library(readr)
+
 qbic_C1DNANuclei <- read_csv("~/Desktop/output/qbic_C1DNANuclei.csv")
 
 #dnaTotalInt <- qbic_C1DNANuclei$Intensity_IntegratedIntensity_Channel1DNA
@@ -9,6 +11,16 @@ qbic_C1DNANuclei <- read_csv("~/Desktop/output/qbic_C1DNANuclei.csv")
 dnaTotalInt <- qbic_C1DNANuclei$Intensity_IntegratedIntensity_Channel1DNACorr
 EdUMeanInt <- qbic_C1DNANuclei$Intensity_MeanIntensity_Channel2EdUCorr
 
-plot(x = dnaTotalInt, y = EdUMeanInt) # basic scatter plot
 
-hist(dnaTotalInt)
+hist(dnaTotalInt, main = "Test 2")
+hist(dnaTotalInt, breaks = (0:50), main = "Test 2")
+
+
+plot(x = dnaTotalInt, y = EdUMeanInt,main = "Test 2") # basic scatter plot
+
+low_vals <- mean(dnaTotalInt < 1)
+high_vals <- mean(dnaTotalInt > 30)
+central_vals <- mean(dnaTotalInt > 1 & dnaTotalInt < 30)
+
+cat(central_vals, "of the values are between 1 and 30; ",low_vals,"<1; and",high_vals,">30")
+
