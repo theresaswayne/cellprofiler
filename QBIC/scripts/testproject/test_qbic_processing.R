34402635ca8dcbae151d91ea5aa2255912b2c94d
theresaswayne <tcs6@columbia.edu>
2018-12-03 15:48:44 -0500

    created r project

---
+++ QBIC/scripts/testproject/test_qbic_processing.R
@@ -0,0 +1,36 @@
+# test_qbic_processing.R
+# for examining CellProfiler data from nuclear analyses
+# adapting to project-based structure
+
+# load packages
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(here)
+
+# load data which you have placed in the data folder
+
+qbic_C1DNANuclei <- read_csv("~/Desktop/output/qbic_C1DNANuclei.csv")
+df <- read.delim(here("data", "raw_foofy_data.csv"))
+p <- ggplot(df, aes(x, y)) + geom_point()
+ggsave(here("figs", "foofy_scatterplot.png"))
+
+
+#dnaTotalInt <- qbic_C1DNANuclei$Intensity_IntegratedIntensity_Channel1DNA
+#EdUMeanInt <- qbic_C1DNANuclei$Intensity_MeanIntensity_Channel2EdU
+dnaTotalInt <- qbic_C1DNANuclei$Intensity_IntegratedIntensity_Channel1DNACorr
+EdUMeanInt <- qbic_C1DNANuclei$Intensity_MeanIntensity_Channel2EdUCorr
+
+
+hist(dnaTotalInt, main = "Test 2")
+hist(dnaTotalInt, breaks = (0:50), main = "Test 2")
+
+
+plot(x = dnaTotalInt, y = EdUMeanInt,main = "Test 2") # basic scatter plot
+
+low_vals <- mean(dnaTotalInt < 1)
+high_vals <- mean(dnaTotalInt > 30)
+central_vals <- mean(dnaTotalInt > 1 & dnaTotalInt < 30)
+
+cat(central_vals, "of the values are between 1 and 30; ",low_vals,"<1; and",high_vals,">30")
+
