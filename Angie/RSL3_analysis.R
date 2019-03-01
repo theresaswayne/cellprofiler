d6bbee9cbcc7902ef11bea16a762a762ccbe745a
Theresa Swayne <tswayne@mac.com>
2019-03-01 17:13:38 -0500

    RSL3 pipeline and R script
    5-pixel-wide membrane
    R script to summarize intensity by treatment

--- Angie/RSL3_analysis.R
+++ Angie/RSL3_analysis.R
@@ -3,4 +3,28 @@
 
 # load your dataset...
 
-cyt_intens <- X2019_03_01_Cytoplasm %>% group_by(Metadata_Treatment) %>% summarise(mean = mean(Intensity_MeanIntensity_Red), intden = mean(Intensity_IntegratedIntensity_Red))
+require(tidyverse)
+require(tcltk)
+
+
+cyt_data <- tk_choose.files(caption = "Select Cytoplasm measurements") %>% 
+  read.csv
+memb_data <- tk_choose.files(caption = "Select Membrane measurements") %>% 
+  read.csv
+
+
+cyt_intens <- cyt_data %>% 
+  group_by(Metadata_Treatment) %>% 
+  summarise(Cyto_Mean = mean(Intensity_MeanIntensity_Red),
+            Cyto_IntDen = mean(Intensity_IntegratedIntensity_Red))
+
+memb_intens <- memb_data %>% 
+  group_by(Metadata_Treatment) %>% 
+  summarise(Memb_Mean = mean(Intensity_MeanIntensity_Red),
+            Memb_IntdDen = mean(Intensity_IntegratedIntensity_Red),
+            Total_Memb_Area = sum(AreaShape_Area))
+
+merged_intens <- left_join(cyt_intens, memb_intens)
+
+write.csv(merged_intens, file = "Results.csv") # in the project directory
+
