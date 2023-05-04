c712c82cdce5b138fdad06d4230f897e6899bacd
Theresa Swayne <theresaswayne@users.noreply.github.com>
2023-05-03 21:42:22 -0400

    Mikko marker analysis

---
+++ 2D_Cell_Analysis/summarize_CP_tables.R
@@ -0,0 +1,39 @@
+# summarize_CP_tables.R
+# R script to gather data from CellProfiler results tables
+# (Object files: cells, cytoplasm, and nuclei)
+# To use: Run the script.
+# Will be prompted for a file
+
+# ---- Setup ----
+require(tidyverse)
+require(readr)
+require(stringr)
+
+
+# ---- Prompt for an object file ----
+# no message will be displayed
+objectFile <- file.choose()
+
+# Read the data from the file
+objectData <- read_csv(objectFile,
+                    locale = locale())
+
+
+mean_intens <- objectData %>% 
+  group_by(Metadata_Experiment, Metadata_Well) %>% 
+  summarise(Mean = mean(Intensity_MeanIntensity_Green),
+            IntDen = mean(Intensity_IntegratedIntensity_Green))
+
+edge_intens <- objectData %>% 
+  group_by(Metadata_Experiment, Metadata_Well) %>% 
+  summarise(Edge_Mean = mean(Intensity_MeanIntensityEdge_Green),
+            Edge_IntDen = mean(Intensity_IntegratedIntensityEdge_Green))
+
+merged_intens <- left_join(mean_intens, edge_intens)
+
+# ---- Save new file ----
+objectName <- str_sub(basename(objectFile), 1, -5) # name of the file without higher levels or extension
+parentDir <- dirname(objectFile) # parent of the logfile
+outputFile = paste(objectName, "_intensity.csv") # spaces will be inserted
+write_csv(merged_intens,file.path(parentDir, outputFile))
+
